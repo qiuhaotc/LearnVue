97093d89aa43496304f5dac86d8e3ac85b2ba453
Owen Qiu <owen.qiu@wisetechglobal.com>
2021-12-20 17:01:47 +0800

    tweaks

---
+++ src/src/bar.js
@@ -0,0 +1,4 @@
+export default function bar() {
+    //
+    console.log("12345")
+}
--- src/src/index.js
+++ src/src/index.js
@@ -1,1 +1,16 @@
-document.write("ABC")
+import bar from './bar.js';
+import _ from 'lodash';
+
+bar();
+
+function component() {
+    const element = document.createElement('div');
+
+    // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+    // lodash 在当前 script 中使用 import 引入
+    element.innerHTML = _.join(['Hello', 'webpack', 'WOW'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
