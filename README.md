d3c55766a38e8902d7c511126b18d030acb7393a
qiuhaotc <qiu66hao@163.com>
2022-01-15 21:04:38 +0800

    delete publish branch files

--- README.md
+++
@@ -1,5 +0,0 @@
-# LearnVue
-
-Learn how to use Vue and webpack
-
-Check this page: <https://qiuhaotc.github.io/LearnVue/src/dist>
--- src/WebApp/scripts/demo.tsx
+++
@@ -1,6 +0,0 @@
-export default class MyClass {
-    AAA: string = "123"
-    SayHi(): void {
-        console.log("Hi, I am " + this.AAA);
-    }
-}
