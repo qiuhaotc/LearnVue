8b1f16065e3d1ab3e58745cb18b2971c5c70c1b8
qiuhaotc <qiu66hao@163.com>
2022-01-15 13:19:14 +0000

    Deploying to publish from @ qiuhaotc/LearnVue@26a88a5922d58dc1e9a1e1e5d8742c657fef9254 🚀

---
+++ src/WebApp/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-container>
+    <el-header>
+      <Header></Header>
+    </el-header>
+    <el-container>
+      <el-aside>
+        <Navi></Navi>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+    <el-footer>
+      <Footer></Footer>
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+import Grid from "./component/Grid.vue";
+import Buttons from "./component/Buttons.vue";
+import Header from "./component/Header.vue";
+import Navi from "./component/Navi.vue";
+import Footer from "./component/Footer.vue";
+import "./css/main.css";
+import MyClass from "./scripts/demo";
+
+var aaa = new MyClass();
+aaa.SayHi();
+
+export default {
+  name: "App",
+  data: function () {
+    return {};
+  },
+  methods: {},
+  components: {
+    Grid,
+    Buttons,
+    Header,
+    Navi,
+    Footer,
+  },
+};
+</script>
---
+++ src/WebApp/component/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="alignToCenter">
+    <el-link href="https://github.com/qiuhaotc/LearnVue" target="_blank"
+      >About this project</el-link
+    >
+  </div>
+</template>
