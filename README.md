f15bf0d64e3fd13b2e9e3d467cb13161e2b37808
qiuhaotc <qiu66hao@163.com>
2022-01-15 21:06:52 +0800

    delete publish branch files

--- README.md
+++
@@ -1,7 +0,0 @@
-## Welcome
-
-### About this project
-
-Learn how to use Vue and webpack
-
-Check this page: <https://qiuhaotc.github.io/LearnVue/src/dist>
--- src/WebApp/component/Footer.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="alignToCenter">
-    <el-link href="https://github.com/qiuhaotc/LearnVue" target="_blank"
-      >About this project</el-link
-    >
-  </div>
-</template>
